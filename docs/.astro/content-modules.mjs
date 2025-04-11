0f906d268f502ea5206a48c9c3ffa29b801746c2
izakikazuho <izaki@mud.co.jp>
2025-04-11 17:31:37 +0900

    update docs

--- docs/.astro/content-modules.mjs
+++ docs/.astro/content-modules.mjs
@@ -1,17 +1,17 @@
 
 export default new Map([
 ["src/content/docs/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/api/animations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Fanimations.mdx&astroContentModuleFlag=true")],
-["src/content/docs/animations/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimations%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/api/better-dialog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Fbetter-dialog.mdx&astroContentModuleFlag=true")],
+["src/content/docs/api/dialog-item.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Fdialog-item.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/api/options.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Foptions.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/api/plugins.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Fplugins.mdx&astroContentModuleFlag=true")],
-["src/content/docs/api/dialog-item.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Fdialog-item.mdx&astroContentModuleFlag=true")],
-["src/content/docs/api/types.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Ftypes.mdx&astroContentModuleFlag=true")],
+["src/content/docs/api/better-dialog.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Fbetter-dialog.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/getting-started/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fgetting-started%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/api/types.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Ftypes.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/ja/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fja%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/api/animations.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fapi%2Fanimations.mdx&astroContentModuleFlag=true")],
+["src/content/docs/animations/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fanimations%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/ja/getting-started/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fja%2Fgetting-started%2Findex.mdx&astroContentModuleFlag=true")],
-["src/content/docs/ja/animations/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fja%2Fanimations%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/plugins/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fplugins%2Findex.mdx&astroContentModuleFlag=true")],
+["src/content/docs/ja/animations/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fja%2Fanimations%2Findex.mdx&astroContentModuleFlag=true")],
 ["src/content/docs/ja/plugins/index.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fdocs%2Fja%2Fplugins%2Findex.mdx&astroContentModuleFlag=true")]]);
 		
